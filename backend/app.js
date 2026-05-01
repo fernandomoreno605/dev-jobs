1e59c9fe2bd2573b6f70cd8221439005d7aafaa7
Fernando Moreno <fernando@withpact.com>
2026-05-01 15:46:52 -0600

    feat: create a basic backend

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import { jobsRouter } from './routes/jobs.js'
+import { corsMiddleware } from './middlewares/cors.js'
+import { DEFAULTS } from './config.js'
+
+const PORT = process.env.PORT ?? DEFAULTS.PORT;
+const app = express();
+
+app.use(corsMiddleware());
+app.use(express.json());
+
+app.use('/jobs', jobsRouter);
+
+app.get('/', (_request, response) => {
+  return response.send("Service running");
+});
+
+app.get('/health', (_request, response) => {
+  return response.status(200).json({
+    uptime: process.uptime(),
+    message: "OK"
+  });
+});
+
+if (!process.env.NODE_ENV) {
+  app.listen(PORT, () => {
+    console.log(`Service running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
